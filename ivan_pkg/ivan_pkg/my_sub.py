5260f0c39d80a33ee27860ea03158cfee5b62aa2
Ayvan77 <ivany7709@gmail.com>
2023-03-08 17:28:50 -0500

    12

--- ivan_pkg/ivan_pkg/my_sub.py
+++ ivan_pkg/ivan_pkg/my_sub.py
@@ -9,7 +9,7 @@ from std_msgs.msg import String
 class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('')
         self.subscription = self.create_subscription(
             String,
             'topic',
