bede69eed18b5a5669e3391f12a2a1c8423c3e3a
Jacksiton <jdicano1319@gmail.com>
2023-03-08 17:49:57 -0500

    chamo

--- chamo_pkg/chamo_pkg/my_pub.py
+++ chamo_pkg/chamo_pkg/my_pub.py
@@ -15,7 +15,7 @@ class MinimalPublisher(Node):
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg.data = 'Identificate chamo: %d' % self.i
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
