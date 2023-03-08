66c3feed1dfd9f2d25c915cdfeef655035a2b4cd
Ayvan77 <ivany7709@gmail.com>
2023-03-08 17:14:09 -0500

    5

--- ivan_pkg/ivan_pkg/rpm.py
+++ ivan_pkg/ivan_pkg/rpm.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Float32
 
 from std_msgs.msg import String
 
@@ -8,17 +9,14 @@ class RPM(Node):
 
     def __init__(self):
         super().__init__('rpm')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(Float32, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Float32()
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
 
 
 def main(args=None):
