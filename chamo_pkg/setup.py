50d62ef26cff7a7f36edb74ce50a83ec144ea581
Jacksiton <jdicano1319@gmail.com>
2023-03-08 17:43:12 -0500

    daw

--- chamo_pkg/setup.py
+++ chamo_pkg/setup.py
@@ -3,24 +3,25 @@ from setuptools import setup
 package_name = 'chamo_pkg'
 
 setup(
- name=package_name,
- version='0.0.0',
- packages=[package_name],
- data_files=[
-     ('share/ament_index/resource_index/packages',
-             ['resource/' + package_name]),
-     ('share/' + package_name, ['package.xml']),
-   ],
- install_requires=['setuptools'],
- zip_safe=True,
- maintainer='TODO',
- maintainer_email='TODO',
- description='TODO: Package description',
- license='TODO: License declaration',
- tests_require=['pytest'],
- entry_points={
-     'console_scripts': [
-             'my_node = my_py_pkg.my_node:main'
-     ],
-   },
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='chamo',
+    maintainer_email='jdicano1319@gmail.com',
+    description='TODO: Package description',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'talker = chamo_pkg.my_pub:main',
+                'listener = chamo_pkg.my_sub:main',
+        ],
+    },
 )
