e6163e734e638ddec918fa3b0b28282a5ba8acc4
Jacksiton <jdicano1319@gmail.com>
2023-03-15 11:24:51 -0500

    champo

--- peruano_pkg/setup.py
+++ peruano_pkg/setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
 
 package_name = 'peruano_pkg'
 
+package_files = [
+    'launch/Tarea.launch.py',
+    'launch/Nodos.launch.py',
+]
+####
 setup(
     name=package_name,
     version='0.0.0',
@@ -10,6 +19,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        #COLOQUE NUEVO 
+        #('share/' + package_name, package_files),
+        #('share/' + package_name + '/launch', package_files),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,6 +36,12 @@ setup(
             'talker = peruano_pkg.jack_pub:main',
             'listener = peruano_pkg.jack_sub:main',
             'jackson = peruano_pkg.prueba:main',
+            'rpm = peruano_pkg.T_PubRpm:main',
+            'suppub = peruano_pkg.T_PubSub:main',
+            'linspeed = peruano_pkg.T_LinSpeed:main',
+            'penviar = peruano_pkg.P_EnviarFoto:main',
+            'precibir = peruano_pkg.P_RecibirFoto:main',
         ],
+        
     },
 )
