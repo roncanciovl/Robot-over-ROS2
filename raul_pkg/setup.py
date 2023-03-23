ddbcccb6aca6ef0c7a0f987c36408b68ee23d9b0
Raul <alvaroraulurdanetag@gmail.com>
2023-03-23 08:09:47 -0500

    alo

--- raul_pkg/setup.py
+++ raul_pkg/setup.py
@@ -27,8 +27,7 @@ setup(
                 'listener = raul_pkg.sub_raul:main',
                 'rpm2ls = raul_pkg.rpm2ls:main',
                 'rpmpub = raul_pkg.pub_rpm:main',
-                'server = raul_pkg.server:main',
-                'client = raul_pkg.client:main'
+                'server = raul_pkg.server:main'
         ],
     },
 )
